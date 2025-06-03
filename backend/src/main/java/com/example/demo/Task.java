6af4372ff4222ae86ec7c6f6094a79ad416c24d0
天ㅊ Noon OωO ㅊ天 <78344868+lunar-noon@users.noreply.github.com>
2025-06-03 15:54:44 +0200

    broken code

--- backend/src/main/java/com/example/demo/Task.java
+++ backend/src/main/java/com/example/demo/Task.java
@@ -1,22 +1,25 @@
 package com.example.demo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
 /** the simplest task 
  * 
  * @author luh
  */
+@Entity
+@Getter @Setter
 public class Task {
 	
+	@Id
+	@GeneratedValue(strategy = jakarta.persistence.GenerationType.AUTO)
+	private Long id;
+
+	@NotBlank
 	private String taskdescription; // must have the EXACT name as his React state property and may not be ignored!
-
-	public Task() {
-    }
-
-	public String getTaskdescription() { // do not apply camel-case here! Its a Bean!
-		return taskdescription;
-	}
-
-	public void setTaskdescription(String taskdescription) { // do not apply camel-case here! Its a Bean!
-		this.taskdescription = taskdescription;
-	}
 
 }
