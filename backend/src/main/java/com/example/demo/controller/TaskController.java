0533fb4c5cf169763a1b883070abbc1d896b8b57
天ㅊ Noon OωO ㅊ天 <78344868+lunar-noon@users.noreply.github.com>
2025-06-17 11:00:55 +0200

    updated taskcontroller

--- backend/src/main/java/com/example/demo/controller/TaskController.java
+++ backend/src/main/java/com/example/demo/controller/TaskController.java
@@ -3,15 +3,14 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Task;
@@ -43,7 +42,7 @@ public class TaskController {
 
 	@CrossOrigin
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> delTask(@RequestParam Long id) {
+	public ResponseEntity<Void> delTask(@PathVariable Long id) {
 		System.out.println("API EP '/delete': '" + id + "'");
     taskRepository.deleteById(id);
 		return ResponseEntity.noContent().build();
